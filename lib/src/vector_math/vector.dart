8dcc026afb189575eb2169e0610ab1a1fb7bc89c
Dominic Hamon <dominic@google.com>
2013-03-25 12:36:00 -0700

    Add assert when out doesn't make sense in cross

--- lib/src/vector_math/vector.dart
+++ lib/src/vector_math/vector.dart
@@ -50,6 +50,7 @@ dynamic cross(dynamic x, dynamic y, [dynamic out=null]) {
   if (x is vec3 && y is vec3) {
     return x.cross(y, out);
   } else if (x is vec2 && y is vec2) {
+    assert(out == null);
     return x.cross(y);
   } else if (x is num && y is vec2) {
     x = x.toDouble();
