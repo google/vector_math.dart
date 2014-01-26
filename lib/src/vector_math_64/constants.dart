c329d3790b53051c5ba3621955f15bdae5283e9b
Kevin Moore <github@j832.com>
2014-01-25 16:21:11 -0800

    trivial analyzer clean-up

--- lib/src/vector_math_64/constants.dart
+++ lib/src/vector_math_64/constants.dart
@@ -18,7 +18,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-part of vector_math;
+part of vector_math_64;
 
 const double degrees2radians = Math.PI/180.0;
 const double radians2degrees = 180.0/Math.PI;
--- test/console_test_harness.dart
+++ test/console_test_harness.dart
@@ -2,10 +2,7 @@ library console_test_harness;
 
 import 'dart:math' as Math;
 import 'package:unittest/unittest.dart';
-import 'package:unittest/vm_config.dart';
 import 'package:vector_math/vector_math.dart';
-import 'package:vector_math/vector_math_lists.dart';
-import 'package:vector_math/vector_math_geometry.dart';
 import 'vector_math_test.dart';
 
 void testCore(Configuration config) {
