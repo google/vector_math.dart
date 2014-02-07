508afc73933d30adb677d30014bfd3de859c847f
Oliver Sand <oliver.sand@tentaclelabs.com>
2014-02-07 15:27:05 +0100

    Add calculateCorners method to Frustum class to retrieve the corners of a Frustum

--- test/test_frustum.dart
+++ test/test_frustum.dart
@@ -52,8 +52,50 @@ class FrustumTest extends BaseTest {
     expect(frustum.intersectsWithSphere(new Sphere.centerRadius(_v3(0.0, 0.0, -101.0), 1.1)), equals(true));
   }
 
+  void testFrustumCalculateCorners() {
+    final Frustum frustum = new Frustum.matrix(
+        makeFrustumMatrix(-1.0, 1.0, -1.0, 1.0, 1.0, 100.0));
+
+    final c0 = new Vector3.zero();
+    final c1 = new Vector3.zero();
+    final c2 = new Vector3.zero();
+    final c3 = new Vector3.zero();
+    final c4 = new Vector3.zero();
+    final c5 = new Vector3.zero();
+    final c6 = new Vector3.zero();
+    final c7 = new Vector3.zero();
+
+    frustum.calculateCorners(c0, c1, c2, c3, c4, c5, c6, c7);
+
+    relativeTest(c0.x, 100.0);
+    relativeTest(c0.y, -100.0);
+    relativeTest(c0.z, -100.0);
+    relativeTest(c1.x, 100.0);
+    relativeTest(c1.y, 100.0);
+    relativeTest(c1.z, -100.0);
+    relativeTest(c2.x, 1.0);
+    relativeTest(c2.y, 1.0);
+    relativeTest(c2.z, -1.0);
+    relativeTest(c3.x, 1.0);
+    relativeTest(c3.y, -1.0);
+    relativeTest(c3.z, -1.0);
+    relativeTest(c4.x, -100.0);
+    relativeTest(c4.y, -100.0);
+    relativeTest(c4.z, -100.0);
+    relativeTest(c5.x, -100.0);
+    relativeTest(c5.y, 100.0);
+    relativeTest(c5.z, -100.0);
+    relativeTest(c6.x, -1.0);
+    relativeTest(c6.y, 1.0);
+    relativeTest(c6.z, -1.0);
+    relativeTest(c7.x, -1.0);
+    relativeTest(c7.y, -1.0);
+    relativeTest(c7.z, -1.0);
+  }
+
   void run() {
     test('Frustum ContainsVector3', testFrustumContainsVector3);
     test('Frustum IntersectsWithSphere', testFrustumIntersectsWithSphere);
+    test('Frustum CalculateCorners', testFrustumCalculateCorners);
   }
 }
