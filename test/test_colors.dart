0cbf63d04b1f14011b5a2399c160207da5ce6cf3
Oliver Sand <oliver.sand@tentaclelabs.com>
2014-02-15 13:13:12 +0100

    Improve tests to check if the input vector can be the same instance as the output vector

--- test/test_colors.dart
+++ test/test_colors.dart
@@ -3,11 +3,11 @@ part of vector_math_test;
 class ColorsTest extends BaseTest {
 
   void testToGrayscale() {
-  	final input = new Vector4(0.0, 1.0, 0.5, 1.0);
-  	final output = new Vector4.zero();
+    final input = new Vector4(0.0, 1.0, 0.5, 1.0);
+    final output = input.clone();
 
-  	Colors.toGrayscale(input, output);
- 	
+    Colors.toGrayscale(output, output);
+   
     relativeTest(output.r, 0.745);
     relativeTest(output.g, 0.745);
     relativeTest(output.b, 0.745);
@@ -15,18 +15,9 @@ class ColorsTest extends BaseTest {
   }
 
   void testHexString() {
-  	final color = new Vector4.zero();
+    final color = new Vector4.zero();
 
-  	Colors.fromHexString('#6495ED', color);
-
-    relativeTest(color.r, 0.3921);
-    relativeTest(color.g, 0.5843);
-    relativeTest(color.b, 0.9294);
-    relativeTest(color.a, 1.0);
-
-    expect(Colors.toHexString(color), equals('6495ed'));
-
-  	Colors.fromHexString('#6495eD', color);
+    Colors.fromHexString('#6495ED', color);
 
     relativeTest(color.r, 0.3921);
     relativeTest(color.g, 0.5843);
@@ -35,7 +26,16 @@ class ColorsTest extends BaseTest {
 
     expect(Colors.toHexString(color), equals('6495ed'));
 
-  	Colors.fromHexString('6495eD', color);
+    Colors.fromHexString('#6495eD', color);
+
+    relativeTest(color.r, 0.3921);
+    relativeTest(color.g, 0.5843);
+    relativeTest(color.b, 0.9294);
+    relativeTest(color.a, 1.0);
+
+    expect(Colors.toHexString(color), equals('6495ed'));
+
+    Colors.fromHexString('6495eD', color);
 
     relativeTest(color.r, 0.3921);
     relativeTest(color.g, 0.5843);
@@ -76,10 +76,10 @@ class ColorsTest extends BaseTest {
   }
 
   void testFromRgba() {
-  	final output = new Vector4.zero();
+    final output = new Vector4.zero();
 
-  	Colors.fromRgba(100, 149, 237, 255, output);
- 	
+    Colors.fromRgba(100, 149, 237, 255, output);
+   
     relativeTest(output.r, 0.3921);
     relativeTest(output.g, 0.5843);
     relativeTest(output.b, 0.9294);
@@ -87,34 +87,36 @@ class ColorsTest extends BaseTest {
   }
 
   void testAlphaBlend() {
-  	final output = new Vector4.zero();
-  	final foreground1 = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
-  	final foreground2 = new Vector4(0.3921, 0.5843, 0.9294, 0.5);
-  	final background1 = new Vector4(1.0, 0.0, 0.0, 1.0);
-  	final background2 = new Vector4(1.0, 0.5, 0.0, 0.5);
+    final output = new Vector4.zero();
+    final foreground1 = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
+    final foreground2 = new Vector4(0.3921, 0.5843, 0.9294, 0.5);
+    final background1 = new Vector4(1.0, 0.0, 0.0, 1.0);
+    final background2 = new Vector4(1.0, 0.5, 0.0, 0.5);
 
-  	Colors.alphaBlend(foreground1, background1, output);
- 	
+    output.setFrom(foreground1);
+    Colors.alphaBlend(output, background1, output);
+   
     relativeTest(output.r, 0.3921);
     relativeTest(output.g, 0.5843);
     relativeTest(output.b, 0.9294);
     expect(output.a, equals(1.0));
 
-  	Colors.alphaBlend(foreground1, background2, output);
+    output.setFrom(background2);
+    Colors.alphaBlend(foreground1, output, output);
 
     relativeTest(output.r, 0.3921);
     relativeTest(output.g, 0.5843);
     relativeTest(output.b, 0.9294);
     expect(output.a, equals(1.0));
 
-  	Colors.alphaBlend(foreground2, background1, output);
+    Colors.alphaBlend(foreground2, background1, output);
 
     relativeTest(output.r, 0.6960);
     relativeTest(output.g, 0.2921);
     relativeTest(output.b, 0.4647);
     expect(output.a, equals(1.0));
 
-  	Colors.alphaBlend(foreground2, background2, output);
+    Colors.alphaBlend(foreground2, background2, output);
 
     relativeTest(output.r, 0.5947);
     relativeTest(output.g, 0.5561);
@@ -123,18 +125,20 @@ class ColorsTest extends BaseTest {
   }
 
   void testLinearGamma() {
-  	final gamma = new Vector4.zero();
-  	final linear = new Vector4.zero();
-  	final foreground = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
+    final gamma = new Vector4.zero();
+    final linear = new Vector4.zero();
+    final foreground = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
 
-  	Colors.linearToGamma(foreground, gamma);
- 	
+    gamma.setFrom(foreground);
+    Colors.linearToGamma(gamma, gamma);
+   
     relativeTest(gamma.r, 0.6534);
     relativeTest(gamma.g, 0.7832);
     relativeTest(gamma.b, 0.9672);
     expect(gamma.a, equals(foreground.a));
 
-  	Colors.gammaToLinear(gamma, linear);
+    linear.setFrom(gamma);
+    Colors.gammaToLinear(linear, linear);
 
     relativeTest(linear.r, foreground.r);
     relativeTest(linear.g, foreground.g);
@@ -143,18 +147,20 @@ class ColorsTest extends BaseTest {
   }
 
   void testRgbHsl() {
-  	final hsl = new Vector4.zero();
-  	final rgb = new Vector4.zero();
-  	final input = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
+    final hsl = new Vector4.zero();
+    final rgb = new Vector4.zero();
+    final input = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
 
-  	Colors.rgbToHsl(input, hsl);
- 	
+    hsl.setFrom(input);
+    Colors.rgbToHsl(hsl, hsl);
+   
     relativeTest(hsl.x, 0.6070);
     relativeTest(hsl.y, 0.7920);
     relativeTest(hsl.z, 0.6607);
     expect(hsl.a, equals(input.a));
 
-  	Colors.hslToRgb(hsl, rgb);
+    rgb.setFrom(hsl);
+    Colors.hslToRgb(rgb, rgb);
 
     relativeTest(rgb.r, input.r);
     relativeTest(rgb.g, input.g);
@@ -163,18 +169,20 @@ class ColorsTest extends BaseTest {
   }
 
   void testRgbHsv() {
-  	final hsv = new Vector4.zero();
-  	final rgb = new Vector4.zero();
-  	final input = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
+    final hsv = new Vector4.zero();
+    final rgb = new Vector4.zero();
+    final input = new Vector4(0.3921, 0.5843, 0.9294, 1.0);
 
-  	Colors.rgbToHsv(input, hsv);
- 	
+    hsv.setFrom(input);
+    Colors.rgbToHsv(hsv, hsv);
+   
     relativeTest(hsv.x, 0.6070);
     relativeTest(hsv.y, 0.5781);
     relativeTest(hsv.z, 0.9294);
     expect(hsv.a, equals(input.a));
 
-  	Colors.hsvToRgb(hsv, rgb);
+    rgb.setFrom(hsv);
+    Colors.hsvToRgb(rgb, rgb);
 
     relativeTest(rgb.r, input.r);
     relativeTest(rgb.g, input.g);
