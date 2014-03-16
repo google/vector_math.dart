660338197731d034d66450a4e569a73fa4e00d59
Oliver Sand <oliver.sand@tentaclelabs.com>
2014-03-16 19:53:22 +0100

    Fix analyser warnings in unit tests

--- test/test_noise.dart
+++ test/test_noise.dart
@@ -9,13 +9,13 @@ class NoiseTest extends BaseTest {
     List<double> values3D = new List<double>(10);
 
     // Cache several values at known coordinates
-    for (int i = 0; i < values2D.length; ++i) {
+    for (var i = 0.0; i < values2D.length; ++i) {
       values2D[i] = noise.noise2D(i, i);
       values3D[i] = noise.noise3D(i, i, i);
     }
 
     // Ensure that querying those same coordinates repeats the cached value
-    for (int i = 0; i < values2D.length; ++i) {
+    for (var i = 0.0; i < values2D.length; ++i) {
       expect(values2D[i], equals(noise.noise2D(i, i)));
       expect(values3D[i], equals(noise.noise3D(i, i, i)));
     }
