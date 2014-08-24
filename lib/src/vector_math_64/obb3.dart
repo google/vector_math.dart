6eb3f71f1ee56a8e362e4f006ee638dc7f932b88
Oliver Sand <oliver.sand@tentaclelabs.com>
2014-08-24 17:05:03 +0200

    Add unittests for obb3 Fix obb3 vs obb3 intersection test Rename obb extents to halfExtents to avoid confusion compared to Aabb3

--- lib/src/vector_math_64/obb3.dart
+++ lib/src/vector_math_64/obb3.dart
@@ -21,18 +21,18 @@
 part of vector_math_64;
 
 /// Defines a 3-dimensional oriented bounding box defined with a [center],
-/// [extents] and axes.
+/// [halfExtents] and axes.
 class Obb3 {
   final Vector3 _center;
-  final Vector3 _extents;
+  final Vector3 _halfExtents;
   final Vector3 _axis0;
   final Vector3 _axis1;
   final Vector3 _axis2;
 
   /// The center of the OBB.
   Vector3 get center => _center;
-  /// The extends of the OBB.
-  Vector3 get extents => _extents;
+  /// The half extends of the OBB.
+  Vector3 get halfExtents => _halfExtents;
   /// The first axis of the OBB.
   Vector3 get axis0 => _axis0;
   /// The second axis of the OBB.
@@ -43,7 +43,7 @@ class Obb3 {
   /// Create a new OBB with erverything set to zero.
   Obb3()
       : _center = new Vector3.zero(),
-        _extents = new Vector3.zero(),
+        _halfExtents = new Vector3.zero(),
         _axis0 = new Vector3(1.0, 0.0, 0.0),
         _axis1 = new Vector3(0.0, 1.0, 0.0),
         _axis2 = new Vector3(0.0, 0.0, 1.0);
@@ -51,16 +51,16 @@ class Obb3 {
   /// Create a new OBB as a copy of [other].
   Obb3.copy(Obb3 other)
       : _center = new Vector3.copy(other._center),
-        _extents = new Vector3.copy(other._extents),
+        _halfExtents = new Vector3.copy(other._halfExtents),
         _axis0 = new Vector3.copy(other._axis0),
         _axis1 = new Vector3.copy(other._axis1),
         _axis2 = new Vector3.copy(other._axis2);
 
-  /// Create a new OBB using [center], [extents] and axis.
-  Obb3.centerExtentsAxes(Vector3 center, Vector3 extents, Vector3 axis0,
+  /// Create a new OBB using [center], [halfExtents] and axis.
+  Obb3.centerExtentsAxes(Vector3 center, Vector3 halfExtents, Vector3 axis0,
       Vector3 axis1, Vector3 axis2)
       : _center = new Vector3.copy(center),
-        _extents = new Vector3.copy(extents),
+        _halfExtents = new Vector3.copy(halfExtents),
         _axis0 = new Vector3.copy(axis0),
         _axis1 = new Vector3.copy(axis1),
         _axis2 = new Vector3.copy(axis2);
@@ -68,7 +68,7 @@ class Obb3 {
   /// Copy from [other] into [this].
   void copyFrom(Obb3 other) {
     _center.setFrom(other._center);
-    _extents.setFrom(other._extents);
+    _halfExtents.setFrom(other._halfExtents);
     _axis0.setFrom(other._axis0);
     _axis1.setFrom(other._axis1);
     _axis2.setFrom(other._axis2);
@@ -77,7 +77,7 @@ class Obb3 {
   /// Copy from [this] into [other].
   void copyInto(Obb3 other) {
     other._center.setFrom(_center);
-    other._extents.setFrom(_extents);
+    other._halfExtents.setFrom(_halfExtents);
     other._axis0.setFrom(_axis0);
     other._axis1.setFrom(_axis1);
     other._axis2.setFrom(_axis2);
@@ -90,23 +90,23 @@ class Obb3 {
 
   /// Rotate [this] by the rotation matrix [t].
   void rotate(Matrix3 t) {
-    t.transform(_axis0..scale(_extents.x));
-    t.transform(_axis1..scale(_extents.y));
-    t.transform(_axis2..scale(_extents.z));
-    _extents.x = _axis0.normalize();
-    _extents.y = _axis1.normalize();
-    _extents.z = _axis2.normalize();
+    t.transform(_axis0..scale(_halfExtents.x));
+    t.transform(_axis1..scale(_halfExtents.y));
+    t.transform(_axis2..scale(_halfExtents.z));
+    _halfExtents.x = _axis0.normalize();
+    _halfExtents.y = _axis1.normalize();
+    _halfExtents.z = _axis2.normalize();
   }
 
   /// Transform [this] by the transform [t].
   void transform(Matrix4 t) {
     t.transform3(_center);
-    t.rotate3(_axis0..scale(_extents.x));
-    t.rotate3(_axis1..scale(_extents.y));
-    t.rotate3(_axis2..scale(_extents.z));
-    _extents.x = _axis0.normalize();
-    _extents.y = _axis1.normalize();
-    _extents.z = _axis2.normalize();
+    t.rotate3(_axis0..scale(_halfExtents.x));
+    t.rotate3(_axis1..scale(_halfExtents.y));
+    t.rotate3(_axis2..scale(_halfExtents.z));
+    _halfExtents.x = _axis0.normalize();
+    _halfExtents.y = _axis1.normalize();
+    _halfExtents.z = _axis2.normalize();
   }
 
   void copyCorner(int cornerIndex, Vector3 corner) {
@@ -118,58 +118,59 @@ class Obb3 {
     {
       case 0:
         corner
-            ..addScaled(_axis0, -_extents.x)
-            ..addScaled(_axis1, -_extents.y)
-            ..addScaled(_axis2, -_extents.z);
+            ..addScaled(_axis0, -_halfExtents.x)
+            ..addScaled(_axis1, -_halfExtents.y)
+            ..addScaled(_axis2, -_halfExtents.z);
         break;
       case 1:
         corner
-            ..addScaled(_axis0, -_extents.x)
-            ..addScaled(_axis1, -_extents.y)
-            ..addScaled(_axis2, _extents.z);
+            ..addScaled(_axis0, -_halfExtents.x)
+            ..addScaled(_axis1, -_halfExtents.y)
+            ..addScaled(_axis2, _halfExtents.z);
         break;
       case 2:
         corner
-            ..addScaled(_axis0, -_extents.x)
-            ..addScaled(_axis1, _extents.y)
-            ..addScaled(_axis2, -_extents.z);
+            ..addScaled(_axis0, -_halfExtents.x)
+            ..addScaled(_axis1, _halfExtents.y)
+            ..addScaled(_axis2, -_halfExtents.z);
         break;
       case 3:
         corner
-            ..addScaled(_axis0, -_extents.x)
-            ..addScaled(_axis1, _extents.y)
-            ..addScaled(_axis2, _extents.z);
+            ..addScaled(_axis0, -_halfExtents.x)
+            ..addScaled(_axis1, _halfExtents.y)
+            ..addScaled(_axis2, _halfExtents.z);
         break;
       case 4:
         corner
-            ..addScaled(_axis0, _extents.x)
-            ..addScaled(_axis1, -_extents.y)
-            ..addScaled(_axis2, -_extents.z);
+            ..addScaled(_axis0, _halfExtents.x)
+            ..addScaled(_axis1, -_halfExtents.y)
+            ..addScaled(_axis2, -_halfExtents.z);
         break;
       case 5:
         corner
-            ..addScaled(_axis0, _extents.x)
-            ..addScaled(_axis1, -_extents.y)
-            ..addScaled(_axis2, _extents.z);
+            ..addScaled(_axis0, _halfExtents.x)
+            ..addScaled(_axis1, -_halfExtents.y)
+            ..addScaled(_axis2, _halfExtents.z);
         break;
       case 6:
         corner
-            ..addScaled(_axis0, _extents.x)
-            ..addScaled(_axis1, _extents.y)
-            ..addScaled(_axis2, -_extents.z);
+            ..addScaled(_axis0, _halfExtents.x)
+            ..addScaled(_axis1, _halfExtents.y)
+            ..addScaled(_axis2, -_halfExtents.z);
         break;
       case 7:
         corner
-            ..addScaled(_axis0, _extents.x)
-            ..addScaled(_axis1, _extents.y)
-            ..addScaled(_axis2, _extents.z);
+            ..addScaled(_axis0, _halfExtents.x)
+            ..addScaled(_axis1, _halfExtents.y)
+            ..addScaled(_axis2, _halfExtents.z);
         break;
     }
   }
 
-  final _r = new Matrix3.zero();
-  final _absR = new Matrix3.zero();
-  final _t = new Vector3.zero();
+  // Avoid allocating these isntance on every call to intersectsWithObb3
+  static final _r = new Matrix3.zero();
+  static final _absR = new Matrix3.zero();
+  static final _t = new Vector3.zero();
 
   /// Check for intersection between [this] and [other].
   bool intersectsWithObb3(Obb3 other, [double epsilon = 1e-3]) {
@@ -178,8 +179,8 @@ class Obb3 {
     _r.setEntry(1, 0, _axis1.dot(other._axis0));
     _r.setEntry(2, 0, _axis2.dot(other._axis0));
     _r.setEntry(0, 1, _axis0.dot(other._axis1));
-    _r.setEntry(2, 1, _axis1.dot(other._axis1));
-    _r.setEntry(3, 1, _axis2.dot(other._axis1));
+    _r.setEntry(1, 1, _axis1.dot(other._axis1));
+    _r.setEntry(2, 1, _axis2.dot(other._axis1));
     _r.setEntry(0, 2, _axis0.dot(other._axis2));
     _r.setEntry(1, 2, _axis1.dot(other._axis2));
     _r.setEntry(2, 2, _axis2.dot(other._axis2));
@@ -206,10 +207,10 @@ class Obb3 {
 
     // Test axes L = A0, L = A1, L = A2
     for (var i = 0; i < 3; i++) {
-      ra = _extents[i];
-      rb = other._extents[0] * _absR.entry(i, 0) +
-          other._extents[1] * _absR.entry(i, 1) +
-          other._extents[2] * _absR.entry(i, 2);
+      ra = _halfExtents[i];
+      rb = other._halfExtents[0] * _absR.entry(i, 0) +
+          other._halfExtents[1] * _absR.entry(i, 1) +
+          other._halfExtents[2] * _absR.entry(i, 2);
 
       if (_t[i].abs() > ra + rb) {
         return false;
@@ -218,10 +219,10 @@ class Obb3 {
 
     // Test axes L = B0, L = B1, L = B2
     for (var i = 0; i < 3; i++) {
-      ra = _extents[0] * _absR.entry(0, i) +
-          _extents[1] * _absR.entry(1, i) +
-          _extents[2] * _absR.entry(2, i);
-      rb = other._extents[i];
+      ra = _halfExtents[0] * _absR.entry(0, i) +
+          _halfExtents[1] * _absR.entry(1, i) +
+          _halfExtents[2] * _absR.entry(2, i);
+      rb = other._halfExtents[i];
 
       if ((_t[0] * _r.entry(0, i) +
            _t[1] * _r.entry(1, i) +
@@ -231,79 +232,79 @@ class Obb3 {
     }
 
     // Test axis L = A0 x B0
-    ra = _extents[1] * _absR.entry(2, 0) + _extents[2] * _absR.entry(1, 0);
-    rb = other._extents[1] * _absR.entry(0, 2) +
-        other._extents[2] * _absR.entry(0, 1);
+    ra = _halfExtents[1] * _absR.entry(2, 0) + _halfExtents[2] * _absR.entry(1, 0);
+    rb = other._halfExtents[1] * _absR.entry(0, 2) +
+        other._halfExtents[2] * _absR.entry(0, 1);
     if ((_t[2] * _r.entry(1, 0) - _t[1] * _r.entry(2, 0)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A0 x B1
-    ra = _extents[1] * _absR.entry(2, 1) + _extents[2] * _absR.entry(1, 1);
-    rb = other._extents[0] * _absR.entry(0, 2) +
-        other._extents[2] * _absR.entry(0, 0);
+    ra = _halfExtents[1] * _absR.entry(2, 1) + _halfExtents[2] * _absR.entry(1, 1);
+    rb = other._halfExtents[0] * _absR.entry(0, 2) +
+        other._halfExtents[2] * _absR.entry(0, 0);
     if ((_t[2] * _r.entry(1, 1) - _t[1] * _r.entry(2, 1)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A0 x B2
-    ra = _extents[1] * _absR.entry(2, 2) + _extents[2] * _absR.entry(1, 2);
-    rb = other._extents[0] * _absR.entry(0, 1) +
-        other._extents[1] * _absR.entry(0, 0);
+    ra = _halfExtents[1] * _absR.entry(2, 2) + _halfExtents[2] * _absR.entry(1, 2);
+    rb = other._halfExtents[0] * _absR.entry(0, 1) +
+        other._halfExtents[1] * _absR.entry(0, 0);
     if ((_t[2] * _r.entry(1, 2) - _t[1] * _r.entry(2, 2)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A1 x B0
-    ra = _extents[0] * _absR.entry(2, 0) + _extents[2] * _absR.entry(0, 0);
-    rb = other._extents[1] * _absR.entry(1, 2) +
-        other._extents[2] * _absR.entry(1, 1);
+    ra = _halfExtents[0] * _absR.entry(2, 0) + _halfExtents[2] * _absR.entry(0, 0);
+    rb = other._halfExtents[1] * _absR.entry(1, 2) +
+        other._halfExtents[2] * _absR.entry(1, 1);
     if ((_t[0] * _r.entry(2, 0) - _t[2] * _r.entry(0, 0)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A1 x B1
-    ra = _extents[0] * _absR.entry(2, 1) + _extents[2] * _absR.entry(0, 1);
-    rb = other._extents[0] * _absR.entry(1, 2) +
-        other._extents[2] * _absR.entry(1, 0);
+    ra = _halfExtents[0] * _absR.entry(2, 1) + _halfExtents[2] * _absR.entry(0, 1);
+    rb = other._halfExtents[0] * _absR.entry(1, 2) +
+        other._halfExtents[2] * _absR.entry(1, 0);
     if ((_t[0] * _r.entry(2, 1) - _t[2] * _r.entry(0, 1)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A1 x B2
-    ra = _extents[0] * _absR.entry(2, 2) + _extents[2] * _absR.entry(0, 2);
-    rb = other._extents[0] * _absR.entry(1, 1) +
-        other._extents[1] * _absR.entry(1, 0);
+    ra = _halfExtents[0] * _absR.entry(2, 2) + _halfExtents[2] * _absR.entry(0, 2);
+    rb = other._halfExtents[0] * _absR.entry(1, 1) +
+        other._halfExtents[1] * _absR.entry(1, 0);
     if ((_t[0] * _r.entry(2, 2) - _t[2] * _r.entry(0, 2)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A2 x B0
-    ra = _extents[0] * _absR.entry(1, 0) + _extents[1] * _absR.entry(0, 0);
-    rb = other._extents[1] * _absR.entry(2, 2) +
-        other._extents[2] * _absR.entry(2, 1);
+    ra = _halfExtents[0] * _absR.entry(1, 0) + _halfExtents[1] * _absR.entry(0, 0);
+    rb = other._halfExtents[1] * _absR.entry(2, 2) +
+        other._halfExtents[2] * _absR.entry(2, 1);
     if ((_t[1] * _r.entry(0, 0) - _t[0] * _r.entry(1, 0)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A2 x B1
-    ra = _extents[0] * _absR.entry(1, 1) + _extents[1] * _absR.entry(0, 1);
-    rb = other._extents[0] * _absR.entry(2, 2) +
-        other._extents[2] * _absR.entry(2, 0);
+    ra = _halfExtents[0] * _absR.entry(1, 1) + _halfExtents[1] * _absR.entry(0, 1);
+    rb = other._halfExtents[0] * _absR.entry(2, 2) +
+        other._halfExtents[2] * _absR.entry(2, 0);
     if ((_t[1] * _r.entry(0, 1) - _t[0] * _r.entry(1, 1)).abs() > ra + rb) {
       return false;
     }
 
     // Test axis L = A2 x B2
-    ra = _extents[0] * _absR.entry(1, 2) + _extents[1] * _absR.entry(0, 2);
-    rb = other._extents[0] * _absR.entry(2, 1) +
-        other._extents[1] * _absR.entry(2, 0);
+    ra = _halfExtents[0] * _absR.entry(1, 2) + _halfExtents[1] * _absR.entry(0, 2);
+    rb = other._halfExtents[0] * _absR.entry(2, 1) +
+        other._halfExtents[1] * _absR.entry(2, 0);
     if ((_t[1] * _r.entry(0, 2) - _t[0] * _r.entry(1, 2)).abs() > ra + rb) {
       return false;
     }
 
     // Since no separating axis is found, the OBBs must be intersecting
-    return false;
+    return true;
   }
 
   //TODO: IntersectsWithQuad
--- test/src/vector_math_64/test_obb3.dart
+++ test/src/vector_math_64/test_obb3.dart
@@ -1,8 +1,279 @@
 library test_obb3_64;
 
 import 'package:unittest/unittest.dart';
+import 'package:vector_math/vector_math_64.dart';
+import 'test_helpers.dart';
+
+void testCorners() {
+  final a = new Obb3()
+      ..center.setValues(0.0, 0.0, 0.0)
+      ..halfExtents.setValues(5.0, 5.0, 5.0);
+  final corner = new Vector3.zero();
+
+  a.copyCorner(0, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0, -5.0, -5.0)));
+
+  a.copyCorner(1, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0, -5.0,  5.0)));
+
+  a.copyCorner(2, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0,  5.0, -5.0)));
+
+  a.copyCorner(3, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0,  5.0,  5.0)));
+
+  a.copyCorner(4, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0, -5.0, -5.0)));
+
+  a.copyCorner(5, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0, -5.0,  5.0)));
+
+  a.copyCorner(6, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0,  5.0, -5.0)));
+
+  a.copyCorner(7, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0,  5.0,  5.0)));
+}
+
+void testTranslate() {
+  final a = new Obb3()
+      ..center.setValues(0.0, 0.0, 0.0)
+      ..halfExtents.setValues(5.0, 5.0, 5.0);
+  final corner = new Vector3.zero();
+
+  a.translate(new Vector3(-1.0, 2.0, 3.0));
+
+  a.copyCorner(0, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0 - 1.0, -5.0 + 2.0, -5.0 + 3.0)));
+
+  a.copyCorner(1, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0 - 1.0, -5.0 + 2.0,  5.0 + 3.0)));
+
+  a.copyCorner(2, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0 - 1.0,  5.0 + 2.0, -5.0 + 3.0)));
+
+  a.copyCorner(3, corner);
+  expect(corner, absoluteEquals(new Vector3(-5.0 - 1.0,  5.0 + 2.0,  5.0 + 3.0)));
+
+  a.copyCorner(4, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0 - 1.0, -5.0 + 2.0, -5.0 + 3.0)));
+
+  a.copyCorner(5, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0 - 1.0, -5.0 + 2.0,  5.0 + 3.0)));
+
+  a.copyCorner(6, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0 - 1.0,  5.0 + 2.0, -5.0 + 3.0)));
+
+  a.copyCorner(7, corner);
+  expect(corner, absoluteEquals(new Vector3( 5.0 - 1.0,  5.0 + 2.0,  5.0 + 3.0)));
+}
+
+void testRotate() {
+  final a = new Obb3()
+      ..center.setValues(0.0, 0.0, 0.0)
+      ..halfExtents.setValues(5.0, 5.0, 5.0);
+  final corner = new Vector3.zero();
+  final matrix = new Matrix3.rotationY(radians(45.0));
+
+  a.rotate(matrix);
+
+  a.copyCorner(0, corner);
+  expect(corner, absoluteEquals(new Vector3(0.0, -5.0, -7.071067810058594)));
+
+  a.copyCorner(1, corner);
+  expect(corner, absoluteEquals(new Vector3(-7.071067810058594, -5.0, 0.0)));
+
+  a.copyCorner(2, corner);
+  expect(corner, absoluteEquals(new Vector3(0.0, 5.0, -7.071067810058594)));
+
+  a.copyCorner(3, corner);
+  expect(corner, absoluteEquals(new Vector3(-7.071067810058594, 5.0, 0.0)));
+
+  a.copyCorner(4, corner);
+  expect(corner, absoluteEquals(new Vector3(7.071067810058594, -5.0, 0.0)));
+
+  a.copyCorner(5, corner);
+  expect(corner, absoluteEquals(new Vector3(0.0, -5.0, 7.071067810058594)));
+
+  a.copyCorner(6, corner);
+  expect(corner, absoluteEquals(new Vector3(7.071067810058594, 5.0, 0.0)));
+
+  a.copyCorner(7, corner);
+  expect(corner, absoluteEquals(new Vector3(0.0, 5.0, 7.071067810058594)));
+}
+
+
+void testTransform() {
+  final a = new Obb3()
+      ..center.setValues(0.0, 0.0, 0.0)
+      ..halfExtents.setValues(5.0, 5.0, 5.0);
+  final corner = new Vector3.zero();
+  final matrix = new Matrix4.diagonal3Values(3.0, 3.0, 3.0);
+
+  a.transform(matrix);
+
+  a.copyCorner(0, corner);
+  expect(corner, absoluteEquals(new Vector3(-15.0, -15.0, -15.0)));
+
+  a.copyCorner(1, corner);
+  expect(corner, absoluteEquals(new Vector3(-15.0, -15.0,  15.0)));
+
+  a.copyCorner(2, corner);
+  expect(corner, absoluteEquals(new Vector3(-15.0,  15.0, -15.0)));
+
+  a.copyCorner(3, corner);
+  expect(corner, absoluteEquals(new Vector3(-15.0,  15.0,  15.0)));
+
+  a.copyCorner(4, corner);
+  expect(corner, absoluteEquals(new Vector3( 15.0, -15.0, -15.0)));
+
+  a.copyCorner(5, corner);
+  expect(corner, absoluteEquals(new Vector3( 15.0, -15.0,  15.0)));
+
+  a.copyCorner(6, corner);
+  expect(corner, absoluteEquals(new Vector3( 15.0,  15.0, -15.0)));
+
+  a.copyCorner(7, corner);
+  expect(corner, absoluteEquals(new Vector3( 15.0,  15.0,  15.0)));
+}
+
+void testIntersectionObb3() {
+  final a = new Obb3()
+      ..center.setValues(0.0, 0.0, 0.0)
+      ..halfExtents.setValues(2.0, 2.0, 2.0);
+
+  final b = new Obb3()
+      ..center.setValues(3.0, 0.0, 0.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final c = new Obb3()
+      ..center.setValues(0.0, 3.0, 0.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final d = new Obb3()
+      ..center.setValues(0.0, 0.0, 3.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final e = new Obb3()
+      ..center.setValues(-3.0, 0.0, 0.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final f = new Obb3()
+      ..center.setValues(0.0, -3.0, 0.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final g = new Obb3()
+      ..center.setValues(0.0, 0.0, -3.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final u = new Obb3()
+      ..center.setValues(1.0, 1.0, 1.0)
+      ..halfExtents.setValues(0.5, 0.5, 0.5);
+
+  final v = new Obb3()
+      ..center.setValues(10.0, 10.0, -10.0)
+      ..halfExtents.setValues(2.0, 2.0, 2.0);
+
+  final w = new Obb3()
+      ..center.setValues(10.0, 0.0, 0.0)
+      ..halfExtents.setValues(1.0, 1.0, 1.0);
+
+  // a - b
+  expect(a.intersectsWithObb3(b), isFalse);
+
+  b.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(b), isTrue);
+
+  b.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(b), isTrue);
+
+  // a - c
+  expect(a.intersectsWithObb3(c), isFalse);
+
+  c.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(c), isTrue);
+
+  c.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(c), isTrue);
+
+  // a - d
+  expect(a.intersectsWithObb3(d), isFalse);
+
+  d.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(d), isTrue);
+
+  d.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(d), isTrue);
+
+  // a - e
+  expect(a.intersectsWithObb3(e), isFalse);
+
+  e.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(e), isTrue);
+
+  e.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(e), isTrue);
+
+  // a - f
+  expect(a.intersectsWithObb3(f), isFalse);
+
+  f.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(f), isTrue);
+
+  f.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(f), isTrue);
+
+  // a - g
+  expect(a.intersectsWithObb3(g), isFalse);
+
+  g.halfExtents.scale(2.0);
+
+  expect(a.intersectsWithObb3(g), isTrue);
+
+  g.rotate(new Matrix3.rotationZ(radians(45.0)));
+
+  expect(a.intersectsWithObb3(g), isTrue);
+
+  // u
+  expect(a.intersectsWithObb3(u), isTrue);
+
+  expect(b.intersectsWithObb3(u), isFalse);
+
+  u.halfExtents.scale(10.0);
+
+  expect(b.intersectsWithObb3(u), isTrue);
+
+  // v
+  expect(a.intersectsWithObb3(v), isFalse);
+
+  expect(b.intersectsWithObb3(v), isFalse);
+
+  // w
+  expect(a.intersectsWithObb3(w), isFalse);
+
+  w.rotate(new Matrix3.rotationZ(radians(22.0)));
+
+  expect(a.intersectsWithObb3(w), isFalse);
+
+  expect(b.intersectsWithObb3(w), isFalse);
+}
 
 void main() {
   group('Obb3', () {
+    test('Corners', testCorners);
+    test('Translate', testTranslate);
+    test('Rotate', testRotate);
+    test('Transforn', testTransform);
+    test('Intersection Obb3', testIntersectionObb3);
   });
 }
