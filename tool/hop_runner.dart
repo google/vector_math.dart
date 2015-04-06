267048459b35ceb6ca881f62ab9fe8d1f3db7b47
Oliver Sand <oliver.sand@tentaclelabs.com>
2015-04-06 22:42:17 +0200

    Replace hop task to generate vector_math_64 with a standalone script Add usage instructions to readme Regenerate vector_math_64, there were some outstanding changes somebody missed Change the documentation link to use dartdocs.org

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -9,19 +9,12 @@ import 'dart:io';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
-import 'generate_vector_math_64_task.dart';
-
 void main(List<String> args) {
 
   //
   // Analyzer
   //
   addTask('analyze_lib', createAnalyzerTask(_getLibs));
-
-  //
-  // Vector Math 64 generation
-  //
-  addTask('generate_vector_math_64', createGenerateVectorMath64Task());
 
   //
   // Hop away!
