e55e67c90c42cfc57aa0f58a188e270fc80b95f1
Oliver Sand <oliver.sand@tentaclelabs.com>
2015-04-06 21:40:12 +0200

    #88 Fix VectorX.normalizeLength

--- lib/src/vector_math/vector4.dart
+++ lib/src/vector_math/vector4.dart
@@ -195,11 +195,11 @@ class Vector4 implements Vector {
     if (l == 0.0) {
       return 0.0;
     }
-    l = 1.0 / l;
-    storage[0] *= l;
-    storage[1] *= l;
-    storage[2] *= l;
-    storage[3] *= l;
+    var d = 1.0 / l;
+    storage[0] *= d;
+    storage[1] *= d;
+    storage[2] *= d;
+    storage[3] *= d;
     return l;
   }
 
