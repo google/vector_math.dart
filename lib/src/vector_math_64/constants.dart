c12e9866dbe7a69951463afbc7954dd039cae487
Oliver Sand <oliver.sand@tentaclelabs.com>
2015-04-07 10:05:30 +0200

    Rename constants back to lowerCase (style guide was changed)

--- lib/src/vector_math_64/constants.dart
+++ lib/src/vector_math_64/constants.dart
@@ -5,16 +5,7 @@
 part of vector_math_64;
 
 /// Constant factor to convert and angle from degrees to radians.
-const double DEGREES_2_RADIANS = Math.PI / 180.0;
+const double degrees2Radians = Math.PI / 180.0;
 /// Constant factor to convert and angle from radians to degrees.
-const double RADIANS_2_DEGREES = 180.0 / Math.PI;
+const double radians2Degrees = 180.0 / Math.PI;
 
-/// DEPRECATED: Use DEGREES_2_RADIANS instead
-@deprecated
-const double degrees2radians = Math.PI / 180.0;
-/// DEPRECATED: Use RADIANS_2_DEGREES instead
-@deprecated
-const double radians2degrees = 180.0 / Math.PI;
-/// DEPRECATED: Use SQRT1_2 from dart:math instead
-@deprecated
-const double sqrtOneHalf = 0.70710678118;
