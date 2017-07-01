07b05badc120ec6ee8d3dd729a52f5c2352c00a0
Alexey Knyazev <lexa.knyazev@gmail.com>
2017-07-01 17:50:28 +0400

    Strong mode and linter clean-up for vector

--- lib/src/vector_math/vector.dart
+++ lib/src/vector_math/vector.dart
@@ -20,16 +20,18 @@ double cross2(Vector2 x, Vector2 y) => x.cross(y);
 
 /// 2D cross product. double x vec2.
 void cross2A(double x, Vector2 y, Vector2 out) {
-  var tempy = x * y.x;
-  out.x = -x * y.y;
-  out.y = tempy;
+  final double tempy = x * y.x;
+  out
+    ..x = -x * y.y
+    ..y = tempy;
 }
 
 /// 2D cross product. vec2 x double.
 void cross2B(Vector2 x, double y, Vector2 out) {
-  var tempy = -y * x.x;
-  out.x = y * x.y;
-  out.y = tempy;
+  final double tempy = -y * x.x;
+  out
+    ..x = y * x.y
+    ..y = tempy;
 }
 
 /// Sets [u] and [v] to be two vectors orthogonal to each other and
@@ -37,24 +39,32 @@ void cross2B(Vector2 x, double y, Vector2 out) {
 void buildPlaneVectors(final Vector3 planeNormal, Vector3 u, Vector3 v) {
   if (planeNormal.z.abs() > Math.SQRT1_2) {
     // choose u in y-z plane
-    double a = planeNormal.y * planeNormal.y + planeNormal.z * planeNormal.z;
-    double k = 1.0 / Math.sqrt(a);
-    u.x = 0.0;
-    u.y = -planeNormal.z * k;
-    u.z = planeNormal.y * k;
-    v.x = a * k;
-    v.y = -planeNormal[0] * (planeNormal[1] * k);
-    v.z = planeNormal[0] * (-planeNormal[2] * k);
+    final double a =
+        planeNormal.y * planeNormal.y + planeNormal.z * planeNormal.z;
+    final double k = 1.0 / Math.sqrt(a);
+    u
+      ..x = 0.0
+      ..y = -planeNormal.z * k
+      ..z = planeNormal.y * k;
+
+    v
+      ..x = a * k
+      ..y = -planeNormal[0] * (planeNormal[1] * k)
+      ..z = planeNormal[0] * (-planeNormal[2] * k);
   } else {
     // choose u in x-y plane
-    double a = planeNormal.x * planeNormal.x + planeNormal.y * planeNormal.y;
-    double k = 1.0 / Math.sqrt(a);
-    u.x = -planeNormal[1] * k;
-    u.y = planeNormal[0] * k;
-    u.z = 0.0;
-    v.x = -planeNormal[2] * (planeNormal[0] * k);
-    v.y = planeNormal[2] * (-planeNormal[1] * k);
-    v.z = a * k;
+    final double a =
+        planeNormal.x * planeNormal.x + planeNormal.y * planeNormal.y;
+    final double k = 1.0 / Math.sqrt(a);
+    u
+      ..x = -planeNormal[1] * k
+      ..y = planeNormal[0] * k
+      ..z = 0.0;
+
+    v
+      ..x = -planeNormal[2] * (planeNormal[0] * k)
+      ..y = planeNormal[2] * (-planeNormal[1] * k)
+      ..z = a * k;
   }
 }
 
