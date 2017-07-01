cefa7ab24d319d2372031d31ae9cdafe39ade42f
Alexey Knyazev <lexa.knyazev@gmail.com>
2017-07-01 14:05:07 +0400

    Strong mode and linter clean-up for generate_vector_math_64

--- tool/generate_vector_math_64.dart
+++ tool/generate_vector_math_64.dart
@@ -8,28 +8,28 @@ import 'dart:io';
 
 import 'package:path/path.dart' as p;
 
-main() async {
+Future<Null> main() async {
   await generateVectorMath64();
 
   print('Generated vector_math_64');
 }
 
-Future generateVectorMath64() async {
-  final directory = new Directory('lib/src/vector_math_64/');
-  final libraryFile = new File('lib/vector_math_64.dart');
+Future<Null> generateVectorMath64() async {
+  final Directory directory = new Directory('lib/src/vector_math_64/');
+  final File libraryFile = new File('lib/vector_math_64.dart');
 
   if (await directory.exists()) {
     await directory.delete(recursive: true);
   }
 
-  if (await libraryFile.exists()) {
+  if (libraryFile.existsSync()) {
     await libraryFile.delete();
   }
 
   await directory.create(recursive: true);
   await _processFile('lib/vector_math.dart');
 
-  await for (var f
+  await for (FileSystemEntity f
       in new Directory('lib/src/vector_math/').list(recursive: true)) {
     if (f is File) {
       await _processFile(f.path);
@@ -37,24 +37,22 @@ Future generateVectorMath64() async {
   }
 }
 
-Future _processFile(String inputFileName) async {
-  final inputFile = new File(inputFileName);
+Future<Null> _processFile(String inputFileName) async {
+  final File inputFile = new File(inputFileName);
 
-  final input = await inputFile.readAsString();
-  final output = _convertToVectorMath64(input);
+  final String input = await inputFile.readAsString();
+  final String output = _convertToVectorMath64(input);
 
-  final outputFileName =
+  final String outputFileName =
       inputFileName.replaceAll('vector_math', 'vector_math_64');
-  var dir = new Directory(p.dirname(outputFileName));
+  final Directory dir = new Directory(p.dirname(outputFileName));
 
   await dir.create(recursive: true);
 
-  final outputFile = new File(outputFileName);
+  final File outputFile = new File(outputFileName);
   await outputFile.writeAsString(output);
 }
 
-String _convertToVectorMath64(String input) {
-  return input
-      .replaceAll('vector_math', 'vector_math_64')
-      .replaceAll('Float32List', 'Float64List');
-}
+String _convertToVectorMath64(String input) => input
+    .replaceAll('vector_math', 'vector_math_64')
+    .replaceAll('Float32List', 'Float64List');
