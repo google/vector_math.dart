0042d7cf0be1bb091c3431f960c78d4e4a4d570f
Jacob MacDonald <jakemac@google.com>
2020-06-22 13:36:08 -0700

    fix some lints

--- lib/src/vector_math_geometry/filters/flat_shade_filter.dart
+++ lib/src/vector_math_geometry/filters/flat_shade_filter.dart
@@ -15,7 +15,7 @@ class FlatShadeFilter extends GeometryFilter {
 
   @override
   MeshGeometry filter(MeshGeometry mesh) {
-    final newAttribs = List<VertexAttrib>.from(mesh.attribs, growable: true);
+    final newAttribs = List<VertexAttrib>.from(mesh.attribs);
 
     if (mesh.getAttrib('NORMAL') == null) {
       newAttribs.add(VertexAttrib('NORMAL', 3, 'float'));
