e13ab6ee1610f122567e26fe840a6ec18efa3bf6
Jacob MacDonald <jakemac@google.com>
2020-06-23 11:37:33 -0700

    prep for merge into master (#222)

--- tool/travis.sh
+++ tool/travis.sh
@@ -8,10 +8,13 @@ if [ "$TRAVIS_DART_VERSION" = "dev" ]; then
   dartfmt -n --set-exit-if-changed .
 fi
 
-dartanalyzer --fatal-warnings --fatal-infos .
+# The lib dir should analyzer without enabling the experiment
+dartanalyzer --fatal-warnings --fatal-infos lib
+# The whole package should analyze with the experiment enabled 
+dartanalyzer --fatal-warnings --fatal-infos --enable-experiment=non-nullable .
 
 # Run the tests.
-pub run test
+pub run --enable-experiment=non-nullable test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
